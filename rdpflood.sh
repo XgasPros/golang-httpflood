0046c35831277c44c94f9e1e6f71359fa6ebe418
XgasPros <119020476+XgasPros@users.noreply.github.com>
2024-04-17 20:28:26 +0700

    Create rdpflood.sh

---
+++ rdpflood.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Step 1: Set the maximum file limit globally
+echo "fs.file-max = 999999" | sudo tee -a /etc/sysctl.conf
+
+# Step 2: Reload the sysctl configuration
+sudo sysctl -p
+
+# Step 3: Run httpflood with the desired parameters
+sudo ./rdpflood "$@"
